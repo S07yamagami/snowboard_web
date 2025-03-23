27e8c865b377cdc82664ccd9a668d7507d6a8d10
yamagami <shanshangmeng@gmail.com>
2025-03-23 19:34:55 +0900

    HP作成イメージ作成

--- resources/views/image.blade.php
+++ resources/views/image.blade.php
@@ -10,18 +10,31 @@
                         <div class="lg:w-1/2 text-center lg:text-left p-6">
                             <h1 class="text-3xl font-bold text-gray-900">HP作成イメージ</h1>
                             <p class="mt-4 text-gray-700">
-                                画像
+                                <strong></strong><br>
+                                <img src="{{ asset('images/home.image.png') }}" alt="トップ画面イメージ" class="my-4 rounded shadow-md"><br>
+                                トップ画面に画像と説明が入れられます！
                                 <br><br>
+
+                                <strong></strong><br>
+                                <img src="{{ asset('images/styles.image.png') }}" alt="スタイル画面" class="my-4 rounded shadow-md"><br>
+                                スタイルの画像が入れられます！
+                                <br><br>
+
+                                <strong></strong><br>
+                                <img src="{{ asset('images/allmountain.image.png') }}" alt="ギア選択" class="my-4 rounded shadow-md"><br>
                                 スノーボード、ビンディング、ブーツが同時に選択できます！
                                 <br><br>
-                                画像
-                                <br><br>
+
+                                <strong></strong><br>
+                                <img src="{{ asset('images/compare.image.png') }}" alt="比較画面" class="my-4 rounded shadow-md"><br>
                                 気になる商品が比較できます！
                                 <br><br>
-                                画像
-                                <br><br>
+
+                                <strong></strong><br>
+                                <img src="{{ asset('images/result.image.png') }}" alt="選択一覧" class="my-4 rounded shadow-md"><br>
                                 選択したスノーボード、ビンディング、ブーツが一覧表示されます！
                             </p>
+
                         </div>
                     </div>
                 </div>
